00169e47da9108d3f416676213129f3ad2a8bdd7
your <uipro.satish@gmail.com>
2020-03-14 20:32:16 -0400

    Updated the ShopData into Firebase, updated the app pulling the data from firebase, but with an execution issue

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,8 @@ const config = {
   measurementId: "G-3MVYZHC8H2"
 };
 
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = async (userAuth, additonalData) => {
   if (!userAuth) return;
 
@@ -39,7 +41,37 @@ export const createUserProfileDocument = async (userAuth, additonalData) => {
   return userRef;
 };
 
-firebase.initializeApp(config);
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
